a3791a203f36db2ad414e35b7e0d9053ae8d4ab3
GrahithMovva <gm4960@rit.edu>
2023-10-24 09:06:56 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+username = "YOUR_CS_USERNAME"
+password = "YOUR_CS_PASSWORD"
+dbName = "YOUR_DB_NAME"
+
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection failed")
