a8913a80e5a47e5573c3d8f873dcab754738daa2
tsg1472 <tsg1472@rit.edu>
2023-10-31 11:18:07 -0400

    CLI design finished

--- cli.py
+++ cli.py
@@ -1,38 +1,74 @@
-# import functions.py
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+import json
 import re
+import funcs
+
+
+credentials = json.load(open("credentials.json"))
+username = credentials["username"]
+password = credentials["password"]
+dbName = "p320_07"
+
+
+def not_implemented(curs):
+    print("Not implemented")
+
 
 functions_info = {
-    "help":     [0, "Usage: help"],
-    "cra":      [5, "Usage: cra  [Email] | [Username] | [Password] | [first_name] | [last_name]"],
-    "addplat":  [1, "Usage: addplat  [platform] "],
-    "crc":      [1, "Usage: crc  [collection_name]"],
-    "coladd":   [2, "Usage: coladd  [collection_name] | [game]"],
-    "coldelg":  [2, "Usage: coldelg  [collection_name] | [game]"],
-    "coldel":   [1, "Usage: coldel  [collection_name]"],
-    "colren":   [2, "Usage: colren  [collection_name] | [new_name]"],
-    "rate":     [2, "Usage: rate  [game] | [stars]"],
-    "fol":      [1, "Usage: fol  [username]"],
-    "unfol":    [1, "Usage: unfol  [username]"],
-    "login":    [2, "Usage: login  [username] | [password]"],
-    "logout":   [0, "Usage: logout"],
+    "cra":      [5, "Usage: cra  [Email] | [Username] | [Password] | [first_name] | [last_name]", funcs.create_account],
+    "addplat":  [1, "Usage: addplat  [platform] ", not_implemented],
+    "crc":      [1, "Usage: crc  [collection_name]", not_implemented],
+    "coladd":   [2, "Usage: coladd  [collection_name] | [game]", not_implemented],
+    "coldelg":  [2, "Usage: coldelg  [collection_name] | [game]", not_implemented],
+    "coldel":   [1, "Usage: coldel  [collection_name]", not_implemented],
+    "colren":   [2, "Usage: colren  [collection_name] | [new_name]", not_implemented],
+    "rate":     [2, "Usage: rate  [game] | [stars]", not_implemented],
+    "fol":      [1, "Usage: fol  [username]", not_implemented],
+    "unfol":    [1, "Usage: unfol  [username]", not_implemented],
+    "login":    [2, "Usage: login  [username] | [password]", not_implemented],
+    "logout":   [0, "Usage: logout", not_implemented],
 }
 
 
-def do_query(string):
-    print("Done!")
-
-
 def authenticate():
-    return None
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+        try:
+            conn = psycopg2.connect(**params)
+            print("Database connection established")
+        except:
+            print("Connection failed")
+        start_session(conn)
 
 
-def start_session():
-    x = ""
-    print("Type help for a list of available functions or 'q' to exit")
-    while x != "q":
+def start_session(conn):
+    curs = conn.cursor()
+    print("Type 'help' for a list of available functions or 'q' to exit")
+    while True:
         names = functions_info.keys()
         x = str(input(">>> "))
+        if x == "q":
+            break
         args = re.split(" ", x)
+
+        if args[0] == "help":
+            print("List of function usages:")
+            for f in names:
+                print(functions_info[f][1])
+            continue
 
         if args[0] not in names:
             print("Invalid function '{}'".format(x))
@@ -43,13 +79,9 @@ def start_session():
             print("Incorrect usage")
             print(func[1])
             continue
-
-        if args[0] == "help":
-            print("List of function usages:")
-            for f in names:
-                print(functions_info[f][1])
-            continue
-        do_query(x)
+        args[0] = curs
+        func[2](args)
+        conn.commit()
 
 
-start_session()
+authenticate()
